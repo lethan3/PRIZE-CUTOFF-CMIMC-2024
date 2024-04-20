8d5792a1dc1ada6d1d1531977fc0e57eb479b8c0
lethan3 <ethanlnow@gmail.com>
2024-04-19 22:01:46 -0700

    strat1 :skull:

--- Don_t Connect 5/bot/strat_1_move.py
+++ Don_t Connect 5/bot/strat_1_move.py
@@ -6,12 +6,14 @@ ALL_NEIGHBOR = lambda x, y, z: ((x + 1, y, z), (x - 1, y, z), (x, y + 1, z), (x,
 SELECT_VALID = lambda lis: [(x, y, z) for (x, y, z) in lis if 1 <= x + y + z <= 2 and -GRID_RADIUS + 1 <= x <= GRID_RADIUS and -GRID_RADIUS + 1 <= y <= GRID_RADIUS and -GRID_RADIUS + 1 <= z <= GRID_RADIUS] # keep those within-bound
 TABLE = {1:0, 2:0, 3:1, 4:3, 5:0}
 
+
 for x in range(-GRID_RADIUS + 1, GRID_RADIUS + 1):
     for y in range(-GRID_RADIUS + 1, GRID_RADIUS + 1):
         for z in range(-GRID_RADIUS + 1, GRID_RADIUS + 1):
             # Check if node is valid
             if 1 <= x + y + z <= 2:
                 node_coordinates.append((x, y, z))
+
 NEIGHBOR_LIST = dict(zip(node_coordinates, [SELECT_VALID(ALL_NEIGHBOR(*(node))) for node in node_coordinates]))
 
 def get_diameter(board, start_node, visit): 
@@ -83,41 +85,78 @@ def eval_move(board_copy, player, pos): # return the value of a move played by p
 
     # see what diameter cc this move would create
     neighbor_diameters = []
-    for ne in ALL_NEIGHBOR[pos]:
-        if (board_copy[ne] == player):
+    for ne in NEIGHBOR_LIST[pos]:
+        if (ne in board_copy and board_copy[ne] == player):
             visit = {pos:0 for pos in node_coordinates}
             neighbor_diameters.append(get_diameter(board_copy, ne, visit))
 
     board_new = board_copy.copy()
     board_new[pos] = player
-    visit = {pos:0 for pos in node_coordinates}
-    new_diameter = get_diameter(board_new, pos, visit)
 
-    if (new_diameter >= 5): return -1
-    if (new_diameter == 4 and max(neighbor_diameters < 4) and sum(neighbor_diameters) != 9): return 5
-    if (new_diameter == 3 and max(neighbor_diameters < 3)): return 4
+    ferr = open('debug.txt', 'a')
+    if (len(board_new) < 16): print(board_new, file=ferr)
+    visit = {p:0 for p in node_coordinates}
+    new_diameter = get_diameter(board_new, pos, visit)
+    print(new_diameter, neighbor_diameters, file=ferr)
+
+    ferr.close()
+
+
+    if (new_diameter == 5): return -1
+    if (new_diameter == 4 and max(neighbor_diameters) < 4 and sum(neighbor_diameters) != 9): return 15
+    if (new_diameter == 3 and max(neighbor_diameters) < 3): return 2
+    if (new_diameter >= 4 and 4 in neighbor_diameters): return -1
+
+    # determine dist 2 score
+
+    val = 0
+    for i in range(3):
+        for j in range(3):
+            if (i == j): continue
+            step_pos = [pos[0], pos[1], pos[2]] # dist 1
+            if (sum(pos) == 1):
+                step_pos[i] += 1
+            else:
+                step_pos[j] -= 1
+            
+            jump_pos = [pos[0], pos[1], pos[2]] # dist 2
+            jump_pos[i] += 1
+            jump_pos[j] -= 1
+
+            step_pos, jump_pos = tuple(step_pos), tuple(jump_pos)
+
+            if (step_pos not in board_copy and jump_pos in board_copy and board_copy[jump_pos] == player):
+                val += 1
     
+    return val
+
 
     
 
 def strat_1_move(board_copy, player):
+    print(len(board_copy))
     
     maxscore = 0
-    select = []
+    moves = []
 
+    max_val = -1e9
     for node in node_coordinates:
         if node not in board_copy:
-            board_new = board_copy.copy()
-            board_new[node] = player
-            s = score(board_new)
-            if s[player] > maxscore:
-                select = [node]
-                maxscore = s[player]
-            elif s[player] == maxscore:
-                select.append(node)
+            val = eval_move(board_copy, player, node)
 
+            print("VAL:", val, file=open('debug.txt', 'a'))
+            if (val < max_val): continue
+            elif (val == max_val):
+                moves.append(node)
+            else:
+                moves.clear()
+                moves.append(node)
+                max_val = val
+
+
+    print('FINAL:', max_val, '\n', file=open('debug.txt', 'a'))
     #print(maxscore)
     #print(select)
-    if len(select)==0:
+    if len(moves)==0:
         return None
-    return random.choice(select)
+    return random.choice(moves)
