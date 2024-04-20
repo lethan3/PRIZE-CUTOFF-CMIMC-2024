bcdb67e306fb2a11160f362bb5c325ee93ddb571
EnchLolz <96092648+EnchLolz@users.noreply.github.com>
2024-04-20 03:25:44 -0700

    added weighted random strategy

--- Blotto Swarm/strategy.py
+++ Blotto Swarm/strategy.py
@@ -10,9 +10,23 @@ NOTE: Each soldier's memory in the final runner will be separate from the others
 WARNING: Do not print anything to stdout. It will break the grading script!
 """
 
-def strategy(ally: list, enemy: list, offset: int) -> int:
+def keep_2_per_castle(ally: list, enemy: list, offset: int) -> int:
     # Implement me!
     return 0
+
+def weighted_random(ally: list, enemy: list, offset: int) -> int:
+    # Implement me!
+    netforce = 0
+    for i in range(len(ally)):
+        if(i!=3): netforce -= ally[i]/((i-3)*abs(1-3))
+        if(i==3): netforce += max(0,ally[3]-2)*(random.random()*2-1)
+    
+    #normalize netforce
+   #print(netforce)
+    netforce += offset
+    if(netforce>1): return 1
+    elif(netforce < -1): return -1
+    else: return 0
 
 def random_strategy(ally: list, enemy: list, offset: int) -> int:
     # A random strategy to use in your game
@@ -28,6 +42,6 @@ def get_strategies():
 
     In the official grader, only the first element of the list will be used as your strategy.
     """
-    strategies = [strategy, random_strategy]
+    strategies = [weighted_random, random_strategy]
 
     return strategies
