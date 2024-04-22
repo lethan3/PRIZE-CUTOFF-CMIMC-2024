0fa2e1f0be1892cd1af2ced6e08cce6c2a671da9
EnchLolz <96092648+EnchLolz@users.noreply.github.com>
2024-04-22 09:37:35 -0700

    added a rng code maker

---
+++ Dungeon Journey/Dungeon Journey starter code/generate_random.py
@@ -0,0 +1,24 @@
+import argparse
+import random
+
+
+
+
+def generate_random(n: int) -> list:
+    file = open("solutions/random.txt", "w")
+    for i in range(n):
+        for j in range(n):
+            instruction = f"{i},{j} "+random.choice([str(i) for i in range(n)]+["_"])+","+random.choice([str(i) for i in range(n)]+["_"])+","+random.choice(["U","D","L","R","_"])+","+random.choice(["U","D","L","R","_"])
+            file.write(instruction+"\n")
+    file.close()
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Generate a random instruction list")
+
+    parser.add_argument("--n", "-n", type=int, default=5)
+    generate_random(parser.parse_args().n)
+
+    
